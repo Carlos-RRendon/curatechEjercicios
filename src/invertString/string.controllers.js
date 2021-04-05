c455ef9163a88ccc0b2ff72477f997503bdc4771
Carlos R. Rendon <carlangass002@hotmail.com>
2021-04-05 12:20:41 -0500

    Funcion de string reverse

--- src/invertString/string.controllers.js
+++ src/invertString/string.controllers.js
@@ -1,13 +1,16 @@
 
+//Objeto que contiene nuestra funcion
 const stringController = {}
 
 stringController.invertString = async (req,res,next) =>{
 
-    const normalString = req.body
+    const normalString = req.body.string;
+    let reversedString = normalString.split('').reverse().join('');
 
-    res.send(normalString);
-
+    res.send(reversedString)
         
 }
 
+
+//Exportamos el modulo
 module.exports = stringController;
